2940b3cb7e884ca5ac948bc3c97fd8634a7f789a
Jonathan Amaya <jonathanfab92@gmail.com>
2023-05-11 13:43:31 +0930

    cognito and new features

--- serverless.ts
+++ serverless.ts
@@ -1,42 +1,46 @@
-import type { AWS } from '@serverless/typescript';
-import graphql from '@functions/graphql';
-import { generateVcard } from '@functions/vcard';
+import type { AWS } from "@serverless/typescript";
+import graphql from "@functions/graphql";
+import { generateVcard } from "@functions/vcard";
+import { signIn, signUp, confirmSignUp } from "@functions/login";
 
 const serverlessConfiguration: AWS = {
-  service: 'contacts-list-sls',
-  frameworkVersion: '3',
+  service: "contacts-list-sls",
+  frameworkVersion: "3",
   plugins: [
     // 'serverless-esbuild',
-    'serverless-webpack',
-    'serverless-offline',
+    "serverless-webpack",
+    "serverless-offline",
   ],
   provider: {
-    name: 'aws',
-    runtime: 'nodejs14.x',
+    name: "aws",
+    runtime: "nodejs14.x",
     apiGateway: {
       minimumCompressionSize: 1024,
       shouldStartNameWithService: true,
     },
     environment: {
-      AWS_NODEJS_CONNECTION_REUSE_ENABLED: '1',
-      NODE_OPTIONS: '--enable-source-maps --stack-trace-limit=1000',
+      AWS_NODEJS_CONNECTION_REUSE_ENABLED: "1",
+      NODE_OPTIONS: "--enable-source-maps --stack-trace-limit=1000",
     },
   },
   functions: {
     graphql,
-    generateVcard
+    generateVcard,
+    signIn,
+    signUp,
+    confirmSignUp,
   },
   package: { individually: true },
   custom: {
     webpack: {
-      webpackConfig: 'webpack.config.js',
+      webpackConfig: "webpack.config.js",
       includeModules: {
-        forceInclude: ['mysql2', 'sequelize-typescript','sequelize'],
+        forceInclude: ["mysql2", "sequelize-typescript", "sequelize"],
       },
     },
-    'serverless-offline': {
-      httpPort: 3001
-    }
+    "serverless-offline": {
+      httpPort: 3001,
+    },
   },
 };
 
